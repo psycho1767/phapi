21f4feb0d4e936cd30bac3c8246ae314566f996b
psycho <77152990+psycho1767@users.noreply.github.com>
2024-06-07 13:04:41 +0000

    a;ll

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
-# start
+from pornhub_api import *
+
+# api = PornhubApi()
+
+
+
+
+
+
+
+
+
+import random
+api = PornhubApi()
+
+tags = random.sample(api.video.tags("f").tags, 5)
+category = random.choice(api.video.categories().categories)
+result = api.search.search_videos(ordering="mostviewed", tags=tags, category=category)
+
+print(result.size())
+for vid in result:
+    print(vid.title, vid.url)
